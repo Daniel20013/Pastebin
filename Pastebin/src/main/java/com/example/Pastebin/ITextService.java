686817de6e2a2d609a4fa4f00338216babb8e69e
Daniel20013 <126924076+Daniel20013@users.noreply.github.com>
2024-02-24 16:02:30 +0200

    Update ITextService.java

--- Pastebin/src/main/java/com/example/Pastebin/ITextService.java
+++ Pastebin/src/main/java/com/example/Pastebin/ITextService.java
@@ -5,5 +5,7 @@ import java.util.List;
 public interface ITextService {
     List<TextTemplate> getText();
 
+    TextTemplate getOneText(Long id);
+
     void createText(TextTemplate textTemplate);
 }
