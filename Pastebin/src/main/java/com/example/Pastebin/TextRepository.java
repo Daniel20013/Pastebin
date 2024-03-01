96c7585c13f241478e6fb3a81204a33af330ca34
Daniel20013 <126924076+Daniel20013@users.noreply.github.com>
2024-03-01 09:32:57 +0200

    Delete Pastebin directory

--- Pastebin/src/main/java/com/example/Pastebin/TextRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Pastebin;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface TextRepository extends JpaRepository<TextTemplate, Long> {
-    Optional<TextTemplate> getTextByText(String text);
-}
